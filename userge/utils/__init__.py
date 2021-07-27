42aa0000732e24e8d507fa62fcdf6aabf8308a14
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-07-27 14:49:45 +0700

    Update __init__.py

--- userge/utils/__init__.py
+++ userge/utils/__init__.py
@@ -8,12 +8,10 @@
 #
 # All rights reserved.
 
-from .aiohttp_helper import AioHttp as get_response
 from .progress import progress  # noqa
-from .sys_tools import SafeDict, get_import_path, check_owner, rand_key, terminate, secure_text  # noqa
+from .sys_tools import SafeDict, get_import_path, terminate, secure_text  # noqa
 from .tools import (sort_file_name_key, # noqa
                     demojify,
-                    sublists,
                     get_file_id_of_media,
                     humanbytes,
                     time_formatter,
